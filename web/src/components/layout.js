aedc368c9c2e7ab8a99e0e85d0c49e9c455dd605
Benjamin Kane <ben@voxel51.com>
2020-03-21 12:25:35 -0400

    layout work

---
+++ web/src/components/layout.js
@@ -0,0 +1,65 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+
+import Header from "./header"
+import "./layout.css"
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const classes = useStyles();
+
+  return (
+    <>
+    <div className={classes.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={3}>
+          <Paper className={classes.paper}>City</Paper>
+        </Grid>
+        <Grid item xs={12} sm={9}>
+          <Paper className={classes.paper}>{children}</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
