966c2ba6e26f429904ba1e5fb610bd859c549a68
Tyler Ganter <tyler@voxel51.com>
2020-03-22 20:19:38 -0400

    reorg

---
+++ tests/start/download_stream.py
@@ -0,0 +1,23 @@
+'''
+# Download Streams
+
+This script allows you to download streams from earthcam.com in real time.
+It will download videos roughly 6 seconds long and save them as `.mp4` files.
+
+In order to get downloads for a new stream, you need to inspect element on the
+videos page and go to the network tab. Look for `chunklist.m3u8` and copy the
+link address to that file.
+This file contains the information of which clips to load.
+The actual clips themselves are the `media_XXXX.ts` files.
+
+
+This script allows you to choose a stream, the path to which can be found by
+inspecting the webpage of the stream and look for an m3u8 file. In this case it
+is chunklist.m3u8
+'''
+from pandemic51.core.streaming import download_stream
+
+output_path = "../out/video"
+
+download_stream("time_square", output_path)
+# download_stream("chicago", output_path)
