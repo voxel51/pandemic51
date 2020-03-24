3c26bee0762bde9edd350424e00aa659a33184a3
Tyler Ganter <tyler@voxel51.com>
2020-03-24 10:13:48 -0400

    config

--- pandemic51/core/database.py
+++ pandemic51/core/database.py
@@ -10,10 +10,10 @@ import pandemic51.core.config as p51c
 
 def connect_database():
     return pymysql.connect(
-        user=p51c.SQL_USERNAME_ENVVAR,
-        password=os.environ.get(p51c.SQL_PASSWORD_ENVVAR),
+        user=p51c.P51_SQL_USERNAME,
+        password=p51c.P51_SQL_PASSWORD,
         host=p51c.SQL_HOST,
-        db=os.environ.get(p51c.SQL_DATABASE_NAME_ENVVAR)
+        db=p51c.P51_SQL_DATABASE_NAME
     )
 
 
--- pandemic51/core/tasks.py
+++ pandemic51/core/tasks.py
@@ -14,14 +14,14 @@ app.config_from_object("pandemic51.core.celery_config")
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    # sender.add_periodic_task(
-    #     pcfg.STREAM_DOWNLOAD_INTERVAL,
-    #     test.s('hello'))
+    sender.add_periodic_task(
+        p51c.STREAM_DOWNLOAD_INTERVAL,
+        test.s('hello'))
 
-    sender.add_periodic_task(
-        p51c.STREAM_DOWNLOAD_INTERVAL, poll_stream.s("time_square"))
-    sender.add_periodic_task(
-        p51c.STREAM_DOWNLOAD_INTERVAL, poll_stream.s("chicago"))
+    # sender.add_periodic_task(
+    #     p51c.STREAM_DOWNLOAD_INTERVAL, poll_stream.s("time_square"))
+    # sender.add_periodic_task(
+    #     p51c.STREAM_DOWNLOAD_INTERVAL, poll_stream.s("chicago"))
 
 
 @app.task
@@ -31,13 +31,10 @@ def test(arg):
 
 @app.task
 def poll_stream(stream_name):
-    import os
     from pandemic51.core.streaming import download_and_store
 
     print("~" * 40)
     print(stream_name)
     print("~" * 40)
 
-    out_dir = os.environ.get("IMAGE_DIR")
-
-    download_and_store(stream_name, out_dir=out_dir)
+    download_and_store(stream_name, out_dir=p51c.IMAGE_DIR)
