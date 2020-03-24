7f9a6300f19edbfd87bb9670060a70e2f28a76e4
Benjamin Kane <ben@voxel51.com>
2020-03-24 12:57:18 -0400

    using area chart

--- web/src/components/chart.js
+++ web/src/components/chart.js
@@ -7,8 +7,11 @@ import CardContent from '@material-ui/core/CardContent';
 
 import {
   ResponsiveContainer,
-  LineChart,
-  Line,
+  AreaChart,
+  Area,
+  stop,
+  defs,
+  linearGradient,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -70,17 +73,25 @@ class Chart extends Component {
       <Card className={classes.root} square>
         <CardContent>
           <ResponsiveContainer width="100%" height={400}>
-        <LineChart
-          data={list}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-  				<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="name" interval="preserveEnd" />
-					<YAxis interval="preserveEnd" />
-					<Legend />
-					<Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-					<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
+<AreaChart width={730} height={250} data={list}
+  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <defs>
+    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+    </linearGradient>
+    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+    </linearGradient>
+  </defs>
+  <XAxis dataKey="name" />
+  <YAxis />
+  <CartesianGrid strokeDasharray="3 3" />
+  <Tooltip />
+  <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+  <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+</AreaChart>
       </ResponsiveContainer>
       </CardContent>
       </Card>
