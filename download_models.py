8ae37c9815bfdeb63b73893caca87b883d91e136
brimoor <brimoor@umich.edu>
2020-03-24 14:28:56 -0400

    only download 1 model for now

--- download_models.py
+++ download_models.py
@@ -27,10 +27,10 @@ def download_model(fid, outpath):
     etau.delete_file(outpath)
 
 
-download_model("1sc3aVx2SIcxar6ah6rg7qiGoecY0m6nK", "models/efficientdet-d0.tar")
-download_model("1xjNuRv58uh3eWy2Hc6KTNfdRNRG8-3rP", "models/efficientdet-d1.tar")
-download_model("15ycqxKVNmi-XEMJgPPShleNxXeDRRQvL", "models/efficientdet-d2.tar")
-download_model("1lHwppExWGE1o-vyE6E1FiadmUIyhdcCA", "models/efficientdet-d3.tar")
+#download_model("1sc3aVx2SIcxar6ah6rg7qiGoecY0m6nK", "models/efficientdet-d0.tar")
+#download_model("1xjNuRv58uh3eWy2Hc6KTNfdRNRG8-3rP", "models/efficientdet-d1.tar")
+#download_model("15ycqxKVNmi-XEMJgPPShleNxXeDRRQvL", "models/efficientdet-d2.tar")
+#download_model("1lHwppExWGE1o-vyE6E1FiadmUIyhdcCA", "models/efficientdet-d3.tar")
 download_model("1OwRBADz6fCdfQGjr57wvTwGOTtzmBld_", "models/efficientdet-d4.tar")
-download_model("1_DZMF_N3MRLeqi34DbPx1Vx7wHtV0rhM", "models/efficientdet-d5.tar")
-download_model("1cFG1kA1U_CinTodRSYtGAH9esZQWsd-w", "models/efficientdet-d6.tar")
+#download_model("1_DZMF_N3MRLeqi34DbPx1Vx7wHtV0rhM", "models/efficientdet-d5.tar")
+#download_model("1cFG1kA1U_CinTodRSYtGAH9esZQWsd-w", "models/efficientdet-d6.tar")
