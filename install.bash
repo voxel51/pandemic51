4b8bc4854aca131c24c98ff2b4c403145dd90393
brimoor <brimoor@umich.edu>
2020-03-24 14:24:04 -0400

    adding density work to install script

--- install.bash
+++ install.bash
@@ -1,16 +1,41 @@
 #!/usr/bin/env bash
+# Installs pandemic51 and its dependencies.
+#
+# Copyright 2020, Voxel51, Inc.
+# voxel51.com
+#
 
-# install m3u8 for streaming
-git clone https://github.com/globocom/m3u8
+# Initialize submodules
+git submodule init
+git submodule update
+
+# Install m3u8
+echo "***** INSTALLING M3U8 *****"
+if [[ ! -d m3u8 ]]; then
+    git clone https://github.com/globocom/m3u8
+fi
 cd m3u8
 python setup.py build
 python setup.py install
 cd -
 
-# install submodules
-pip install -r eta/requirements.txt
-pip install -e eta
+# Install ETA
+echo "***** INSTALLING ETA *****"
+cd eta
+bash install.bash
+if [[ ! -f config.json ]]; then
+    cp config-example.json config.json
+fi
+cd ..
 
-# install pandemic51
+# Install pandemic51
+echo "***** INSTALLING PANDEMIC51 *****"
 pip install -e .
 pip install -r requirements.txt
+
+# Install COCO API (required by `automl/efficientdet`)
+echo "***** INSTALLING COCO API *****"
+git clone https://github.com/cocodataset/cocoapi.git
+make --directory=cocoapi/PythonAPI
+cp -r cocoapi/PythonAPI/pycocotools eta/tensorflow/models/research/
+rm -rf cocoapi
