78ed2a75435100c6ded410bf330ff5b0886b04af
Tyler Ganter <tyler@voxel51.com>
2020-03-24 12:28:17 -0400

    celery stop

--- celerystop.bash
+++ celerystop.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo systemctl restart celery.service celerybeat.service
+sudo systemctl stop celery.service celerybeat.service
 sudo systemctl status celery.service celerybeat.service
