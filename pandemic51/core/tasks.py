2bba751fbe3094eb44556642ff74aadd44e60b3d
Tyler Ganter <tyler@voxel51.com>
2020-03-23 20:57:04 -0400

    initial test

--- pandemic51/core/tasks.py
+++ pandemic51/core/tasks.py
@@ -3,6 +3,9 @@ Celery Tasks
 
 '''
 import celery
+# from celery.schedules import crontab
+#
+# import pandemic51.core.config as pcfg
 
 # app = celery.Celery("pandemic51.tasks")
 # app.config_from_object("pandemic51.celery_config")
@@ -13,17 +16,28 @@ app = celery.Celery(
     backend='redis://localhost'
 )
 
+
+@celery.signals.celeryd_init.connect()
+def run_on_startup(sender=None, conf=None, **kwargs):
+    ''' Execute these other tasks on startup, either as one time or very
+    infrequent tasks that should run after system comes online
+    '''
+    add(3, 5).delay()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Setup job refresh task
+    # sender.add_periodic_task(mcfg.PLATFORM_JOB_POLL_RATE, refresh_all_jobs.s(),
+    #                          name='refresh all jobs')
+    pass
+
+
 @app.task
 def add(x, y):
     return x + y
 
-# import celery
-# from celery.schedules import crontab
-#
-# import pandemic51.core.config as pcfg
-#
-# app = celery.Celery()
-#
+
 # @app.on_after_configure.connect
 # def setup_periodic_tasks(sender, **kwargs):
 #     # Calls test('hello') every 10 seconds.
