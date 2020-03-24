720d3c02d05e99f93d15f182493a6a82317c4013
Tyler Ganter <tyler@voxel51.com>
2020-03-24 19:41:05 -0400

    adf

--- tests/density_for_db/test.py
+++ tests/density_for_db/test.py
@@ -1,14 +1,14 @@
 '''
-#
-# '''
-# import pandemic51.core.database as pand
-#
-# import pandemic51.core.density as pande
-#
-# result = pand.query_unprocessed_images()
-#
-# print(result)
-#
-# pand.add_stream_labels(1, "ASDF")
-#
-# # pande.compute_density_for_unprocessed_images()
+
+'''
+import pandemic51.core.database as pand
+
+import pandemic51.core.density as pande
+
+result = pand.query_unprocessed_images()
+
+print(result)
+
+pand.add_stream_labels(1, "ASDF")
+
+# pande.compute_density_for_unprocessed_images()
