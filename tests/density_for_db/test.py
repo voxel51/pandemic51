9031464f89c91af0c8e003553e81fcbf83b2e950
Tyler Ganter <tyler@voxel51.com>
2020-03-25 11:00:15 -0400

    documenting

--- tests/density_for_db/test.py
+++ tests/density_for_db/test.py
@@ -1,12 +1,12 @@
 '''
+Test that computes detections for all entries in the database that do not have
+a `labels_path` column populated. The column is populated with the path to the
+labels on disk.
 
+Copyright 2020 Voxel51, Inc.
+voxel51.com
 '''
-import pandemic51.core.database as pand
-
 import pandemic51.core.density as pande
 
-# result = pand.query_unprocessed_images()
-# print(result)
-# pand.add_stream_labels(1, "ASDF")
 
 pande.compute_density_for_unprocessed_images()
