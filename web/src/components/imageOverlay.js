b5b288bbbd443744e65f775d32ed0c4b3bdd4aeb
alan <alan@voxel51.com>
2020-03-26 16:48:35 -0400

    Track loading state

--- web/src/components/imageOverlay.js
+++ web/src/components/imageOverlay.js
@@ -4,12 +4,26 @@ import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
 export default function ImageOverlay({src, onClose}) {
+  const [isLoaded, setLoaded] = useState(false);
+
+  useEffect(() => setLoaded(false), [src]);
+
+  const handleLoad = () => {
+    console.log('loaded!')
+    setLoaded(true);
+  };
+
   if (!src) {
     return null;
   }
+
+  const imageStyle = {
+    display: isLoaded ? 'block' : 'none',
+  };
+
   return (
     <div className="image-overlay">
-      <img src={src}/>
+      <img src={src} onLoad={handleLoad} style={imageStyle}/>
       <IconButton aria-label="close" className="close-button" onClick={onClose}>
         <CloseIcon />
       </IconButton>
