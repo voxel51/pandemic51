1a52ba8896cddc8b7f2d7b6ddf4fc131e53ec556
brimoor <brimoor@umich.edu>
2020-03-27 15:25:59 -0400

    cleaning up api

--- api/api.py
+++ api/api.py
@@ -1,10 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
-import pandemic51.core.database as pand
-import pandemic51.core.config as panc
+'''
+Main app runner.
+
+Copyright 2020, Voxel51, Inc.
+voxel51.com
+'''
+from datetime import datetime
 import pandas as pd
 import time
-import datetime
+
+from flask import Flask
+app = Flask(__name__)
+
+import pandemic51.core.config as panc
+import pandemic51.core.database as pand
+
 
 NAMES = {
     "london": "abbey_road",
@@ -16,8 +25,10 @@ NAMES = {
     "neworleans": "new_orleans",
 }
 
+US = {"chicago", "newyork", "neworleans", "newjersey"}
 
-US = { "chicago", "newyork", "neworleans", "newjersey" }
+EVENTS_CSV_PATH = "./sheets.csv"
+
 
 @app.route("/snapshots")
 def snapshots():
@@ -27,7 +38,12 @@ def snapshots():
     for i in r:
         if i["city"] not in inv:
             continue
-        res[inv[i["city"]]] = "https://pdi-service.voxel51.com/snapshots" + i["url"].replace(panc.P51_BASEDIR + "/data", "")
+
+        res[inv[i["city"]]] = (
+            "https://pdi-service.voxel51.com/snapshots" +
+            i["url"].replace(panc.P51_BASEDIR + "/data", "")
+        )
+
     return {"data": res}
 
 
@@ -35,43 +51,48 @@ def snapshots():
 def pdi(city):
     if city not in NAMES:
         return 404, "Not Found"
-    raw = pd.read_csv("./sheets.csv")
+
+    raw = pd.read_csv(EVENTS_CSV_PATH)
     events = []
-    for date, City, event, ref in zip(raw["date"], raw["city"], raw["event"], raw["reference"]):
-        if City.strip(" ") == "U.S." and city in US:
+    for date, city_, event, ref in zip(
+            raw["date"], raw["city"], raw["event"], raw["reference"]):
+        if city_.strip(" ") == "U.S." and city in US:
             pass
-        elif City.strip(" ") == "Worldwide":
+        elif city_.strip(" ") == "Worldwide":
             pass
-        elif City != city:
+        elif city_ != city:
             continue
+
         ref = str(ref)
         if ref == "nan":
             continue
+
         event = event.strip('"')
         events.append({
-            "time": time.mktime(datetime.datetime.strptime(date, "%m/%d/%Y").timetuple()),
+            "time": time.mktime(
+                datetime.strptime(date, "%m/%d/%Y").timetuple()),
             "event": event,
-            "reference": ref
+            "reference": ref,
         })
-    devents = {
-      int(e["time"]): e for e in events
-    }
+
+    devents = {int(e["time"]): e for e in events}
     levents = [e["time"] for e in events]
     points = pand.query_stream_pdi(NAMES[city])
 
-    def closest(lst, K): 
-        return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
+    def closest(lst, K):
+        return lst[min(range(len(lst)), key=lambda i: abs(lst[i] - K))]
 
     for p in points:
-        p["url"] = "https://pdi-service.voxel51.com/snapshots" + p["url"].replace(panc.P51_BASEDIR + "/data", "")
+        p["url"] = (
+            "https://pdi-service.voxel51.com/snapshots" +
+            p["url"].replace(panc.P51_BASEDIR + "/data", "")
+        )
         p["event"] = closest(levents, p["time"])
         p["event_val"] = 0
 
-    
-    labels = { p["time"]: p for p in points }
+    labels = {p["time"]: p for p in points}
 
-    return { "data": points, "events": devents, "labels" : labels }
-
+    return {"data": points, "events": devents, "labels" : labels}
 
 
 if __name__ == "__main__":
