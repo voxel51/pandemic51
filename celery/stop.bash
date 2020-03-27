327f71c3f8ca08472b9471fa53f6676b3dfdfad8
brimoor <brimoor@umich.edu>
2020-03-27 10:48:55 -0400

    updating copyrights

--- celery/stop.bash
+++ celery/stop.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+# Stops the Celery service.
+#
+# Copyright 2020, Voxel51, Inc.
+# voxel51.com
+#
+
 sudo systemctl stop celery.service celerybeat.service
 sudo systemctl status celery.service celerybeat.service
