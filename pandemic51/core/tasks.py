940ea49efa4c73f075ee0f3b40968a44c15567ab
brimoor <brimoor@umich.edu>
2020-03-26 20:48:03 -0400

    adding docstrings

--- pandemic51/core/tasks.py
+++ pandemic51/core/tasks.py
@@ -19,28 +19,31 @@ app.config_from_object("pandemic51.core.celery_config")
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
+    '''Setup periodic Celery tasks.'''
     for stream_name in panc.STREAMS:
         sender.add_periodic_task(
             panc.STREAM_DOWNLOAD_INTERVAL, das_task.s(stream_name))
-        sender.add_periodic_task(panc.DENSITY_COMPUTE_INTERVAL, dofui_task.s())
-        sender.add_periodic_task(panc.DENSITY_COMPUTE_INTERVAL, ccfde_task.s())
+        sender.add_periodic_task(
+            panc.DENSITY_COMPUTE_INTERVAL, dofaui_task.s())
+        sender.add_periodic_task(
+            panc.DENSITY_COMPUTE_INTERVAL, cocfade_task.s())
 
 
 @app.task()
 def das_task(stream_name):
-    '''"Task for downloading and storing images.'''
+    '''Task for downloading and storing images.'''
     tmpdirbase = os.path.join(panc.DATA_DIR, "tmp")
     pans.download_and_store(
         stream_name, out_dir=panc.IMAGES_DIR, tmpdirbase=tmpdirbase)
 
 
 @app.task()
-def dofui_task():
-    '''"Task for detecting objects for all unprocessed images.'''
+def dofaui_task():
+    '''Task for detecting objects for all unprocessed images.'''
     pand.detect_objects_in_unprocessed_images()
 
 
 @app.task()
-def ccfde_task():
-    '''Task for computing object counts for all non-null DB entries.'''
+def cocfade_task():
+    '''Task for computing object counts for all DB entries.'''
     pand.compute_object_counts_for_db_entries()
