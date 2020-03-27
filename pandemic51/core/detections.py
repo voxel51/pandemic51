1e5708205821ca718391a82347c968d6e180e86a
brimoor <brimoor@umich.edu>
2020-03-27 10:25:03 -0400

    store object count while processing the images

--- pandemic51/core/detections.py
+++ pandemic51/core/detections.py
@@ -46,11 +46,9 @@ def detect_objects_in_images(inpaths, outpaths):
 
 
 def detect_objects_in_unprocessed_images():
-    '''Detects objects in all images in the DB that don't have labels, and
-    stores the following things for each image:
-        - the ImageLabels to disk
-        - the annotated image to disk
-        - the labels path to the DB
+    '''Detects objects in all images in the database that don't have labels,
+    and records the object counts, ImageLabels, and annotated images for each
+    of them.
     '''
     # Get the full list of unprocessed images
     unprocessed_images = pand.query_unprocessed_images()
@@ -77,35 +75,12 @@ def detect_objects_in_unprocessed_images():
                 # Another worker processed this image, so skip
                 continue
 
-            _process_image(
+            count = _process_image(
                 detector, image_path, labels_path, anno_path=anno_path)
 
+            pand.set_object_count(id, count)
             pand.add_stream_labels(id, labels_path)
             pand.add_stream_anno_img(id, anno_path)
-
-
-def compute_object_counts_for_db_entries(recompute_all=False):
-    '''Computes object counts for all non-null (by default) DB entries and
-    stores the results.
-
-    Args:
-        recompute_all: whether to recompute all object counts. By default, this
-            is False
-    '''
-    cnx = pand.connect_database()
-    rows = pand.query_stream_history(cnx=cnx)
-
-    for id, _, _, _, labels_path, count in rows:
-        if not labels_path:
-            continue
-
-        if count is not None and not recompute_all:
-            continue
-
-        image_labels = etai.ImageLabels.from_json(labels_path)
-        count = len(image_labels.objects)
-
-        pand.populate_object_count(id, count, cnx=cnx)
 
 
 def _load_efficientdet_model(model_name):
@@ -153,3 +128,5 @@ def _process_image(detector, img_path, labels_path, anno_path=None):
         img_anno = etaa.annotate_image(
             img, image_labels, annotation_config=ANNOTATION_CONFIG)
         etai.write(img_anno, anno_path)
+
+    return count
