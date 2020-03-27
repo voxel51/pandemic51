e180c02b186335610d936daf7b736850498fc16d
brimoor <brimoor@umich.edu>
2020-03-26 22:29:43 -0400

    removing py2 stuff

--- pandemic51/detectors/efficientdet.py
+++ pandemic51/detectors/efficientdet.py
@@ -5,18 +5,6 @@ at https://github.com/google/automl/tree/master/efficientdet.
 Copyright 2017-2020 Voxel51, Inc.
 voxel51.com
 '''
-# pragma pylint: disable=redefined-builtin
-# pragma pylint: disable=unused-wildcard-import
-# pragma pylint: disable=wildcard-import
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-from builtins import *
-# pragma pylint: enable=redefined-builtin
-# pragma pylint: enable=unused-wildcard-import
-# pragma pylint: enable=wildcard-import
-
 import logging
 import os
 import sys
