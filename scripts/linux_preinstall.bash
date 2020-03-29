54f20e8e4b491659f0bf6330467f8a2d3505eaf0
brimoor <brimoor@umich.edu>
2020-03-29 01:05:38 -0400

    moving preinstall scripts to scripts/

--- scripts/linux_preinstall.bash
+++ scripts/linux_preinstall.bash
@@ -8,15 +8,12 @@
 sudo apt update
 sudo apt upgrade
 
-sudo snap install ffmpeg
-
 sudo apt install unzip
 sudo apt install git python-setuptools python-dev python-virtualenv curl rabbitmq-server
 sudo apt install mysql-server mysql-client libmysqlclient-dev
 sudo apt install redis-server
 redis-server --daemonize yes
 
-########### Python and PIP
 #
 # Install Python 3.6, pip, and virtualenv
 #
@@ -25,7 +22,7 @@ redis-server --daemonize yes
 # These instructions assume you are using Ubuntu 16.04, which does not come
 # with Python 3.6 by default. More recent versions of Ubuntu may have
 # Python 3.6 installed by default
-echo "--- INSTALLING PYTHON 3.6 ---"
+#
 sudo apt-get -y --no-install-recommends install software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
@@ -35,21 +32,8 @@ curl https://bootstrap.pypa.io/get-pip.py | sudo python
 sudo pip install --upgrade pip setuptools
 sudo pip install virtualenv
 
-mkdir -p $ENV_DIR
-
-if [ -z "$ENV_DIR" ]; then
-    ENV_DIR=${HOME}/venv
-    export ENV_DIR
-    echo "setting virtual environment directory to ${ENV_DIR}"
-fi
-cd ${ENV_DIR}
-virtualenv -p /usr/bin/python3.6 covid19
-cd -
-
-########### Celery
-mkdir -p $CELERY_DIR
-
-########### Data store
-mkdir -p $DATA_DIR
-mkdir -p $IMAGE_DIR
-mkdir -p $LABELS_DIR
+# Initialize directories
+mkdir -p "${P51_CELERY_DIR}"
+mkdir -p "${P51_DATA_DIR}/images"
+mkdir -p "${P51_DATA_DIR}/labels"
+mkdir -p "${P51_DATA_DIR}/anno"
