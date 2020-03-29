22e212eb3cc73194cb7efc9de0215d82e390a4ba
brimoor <brimoor@umich.edu>
2020-03-29 01:05:11 -0400

    moving sql init to scripts/

--- scripts/init_db.bash
+++ scripts/init_db.bash
@@ -5,7 +5,7 @@
 # voxel51.com
 #
 
-echo "Configuring MYSQL"
+echo "***** CONFIGURING MYSQL *****"
 
 sudo mysql -u root -e "DROP DATABASE IF EXISTS $P51_SQL_DATABASE_NAME;"
 sudo mysql -u root -e "CREATE DATABASE $P51_SQL_DATABASE_NAME;"
@@ -15,4 +15,4 @@ sudo mysql -u root -e "CREATE USER '$P51_SQL_USERNAME'@'localhost' IDENTIFIED BY
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $P51_SQL_DATABASE_NAME.* TO '$P51_SQL_USERNAME'@'localhost';"
 sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysql -u $P51_SQL_USERNAME -p$P51_SQL_PASSWORD $P51_SQL_DATABASE_NAME < database/init_tables.sql
+mysql -u $P51_SQL_USERNAME -p$P51_SQL_PASSWORD $P51_SQL_DATABASE_NAME < scripts/init_tables.sql
