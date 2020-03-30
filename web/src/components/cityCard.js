8be94795fe3c5e5a761a5c64d31ae60fe68d1746
Benjamin Kane <ben@voxel51.com>
2020-03-30 11:07:27 -0400

    city card cleanup

--- web/src/components/cityCard.js
+++ web/src/components/cityCard.js
@@ -4,21 +4,20 @@
  * Copyright 2020, Voxel51, Inc.
  * voxel51.com
  */
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Clock from 'react-live-clock';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import { Link } from "gatsby";
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import {Link} from 'gatsby';
+import React from 'react';
+import Clock from 'react-live-clock';
+
 const useStyles = makeStyles({
   root: {
     marginBottom: '1rem',
-    overflow: "visible",
+    overflow: 'visible',
   },
   bullet: {
     display: 'inline-block',
@@ -31,54 +30,59 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-  content: {
-    paddingRight: (107 * 16/9) + 8
-  },
+  content: {paddingRight: (107 * 16 / 9) + 8},
   still: {
     margin: 0,
     right: 0,
-    width: 107 * 16/9,
+    width: 107 * 16 / 9,
     height: 107,
-    position: "absolute",
+    position: 'absolute',
   },
   chip: {
-    position: "absolute",
+    position: 'absolute',
     right: 0,
     bottom: 0,
-    margin: "0 .5rem .5rem 0",
-    padding: "0.5rem"
+    margin: '0 .5rem .5rem 0',
+    padding: ',0.5rem'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
   }
 });
 
 const timezones = {
-  "chicago": "America/Chicago",
-  "dublin": "Europe/Dublin",
-  "fortlauderdale": "America/New_York",
-  "london": "Europe/London",
-  "neworleans": "America/Chicago",
-  "newjersey": "America/New_York",
-  "newyork": "America/New_York",
-  "prague": "Europe/Prague"
+  'chicago': 'America/Chicago',
+  'dublin': 'Europe/Dublin',
+  'fortlauderdale': 'America/New_York',
+  'london': 'Europe/London',
+  'neworleans': 'America/Chicago',
+  'newjersey': 'America/New_York',
+  'newyork': 'America/New_York',
+  'prague': 'Europe/Prague'
 }
 
 const locations = {
-  "chicago": "Wrigley Field",
-  "dublin": "Temple Bar",
-  "london": "Abbey Road",
-  "neworleans": "Bourbon Street",
-  "newjersey": "Seaside Heights",
-  "newyork": "Times Square",
-  "prague": "Grand Hotel",
-  "fortlauderdale": "Wind Jammer"
+  'chicago': 'Wrigley Field',
+  'dublin': 'Temple Bar',
+  'london': 'Abbey Road',
+  'neworleans': 'Bourbon Street',
+  'newjersey': 'Seaside Heights',
+  'newyork': 'Times Square',
+  'prague': 'Grand Hotel',
+  'fortlauderdale': 'Wind Jammer'
 }
 
-export default function CityCard(props) {
+export default function
+CityCard(props) {
   const classes = useStyles();
   const active = props.cityId === props.active && props.cityId !== undefined;
+  const bull = <span className={classes.bullet}>•</span>
   return (
-    <Card className={classes.root + (active ? " active-card" : "")} square>
+    <Card className={classes.root + (active ? ' active-card' : '')} square>
       <CardActionArea>
-        <Link to={"/" + props.cityId}>
+        <Link to={'/' + props.cityId}>
           <CardMedia
             className={classes.still}
             image={props.url}
@@ -88,11 +92,11 @@ export default function CityCard(props) {
             <Typography variant="h5" component="h2">
               {props.name}
             </Typography>
-            <Typography variant="h6" component="h3" className={classes.pos} color="textSecondary">
+            <Typography variant='h6' component='h3' className={classes.pos} color='textSecondary'>
               <Clock format={'hh:mm:ss A'} ticking={true} timezone={timezones[props.cityId]} />
             </Typography>
-            <Typography variant="body1" component="p">
-              {locations[props.cityId]}
+            <Typography variant='body1' component='p'>
+              {locations[props.cityId]} {bull} +10%
             </Typography>
           </CardContent>
         </Link>
