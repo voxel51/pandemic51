54ff189f525abe15d93e66d32edc90d347b23f8b
brimoor <brimoor@umich.edu>
2020-03-30 13:16:51 -0400

    converting to public GDrive link for downloading historical data

--- scripts/download_historical_data.bash
+++ scripts/download_historical_data.bash
@@ -5,13 +5,6 @@
 # voxel51.com
 #
 
-
-# Download images
-eta gdrive download-dir 1RE-JMlKLYpQbfOkbwMFRhr_xaQ7ULVl_ data/historical/images
-unzip 'data/historical/images/*.zip' -d data/historical/images
-rm data/historical/images/*.zip
-
-# Download labels
-eta gdrive download-dir 1TYRgb-4JoKL8fmEqBhd5zOC3Lhs6VwOD data/historical/labels
-unzip 'data/historical/labels/*.zip' -d data/historical/labels
-rm data/historical/labels/*.zip
+eta gdrive download --public 1zxzxBz0DyKvETMUvB2tTlVZd4b4kPttB data/historical.tar.gz
+tar -xf data/historical.tar.gz -C data/
+rm data/historical.tar.gz
