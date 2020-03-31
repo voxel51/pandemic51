24ff75a3eccfcd90c9ec2a945cca3a7a959d51d5
Eric Hofesmann <ehofesmann@gmail.com>
2020-03-31 10:29:46 -0400

    Apt install chrome-browser

--- scripts/linux_preinstall.bash
+++ scripts/linux_preinstall.bash
@@ -12,6 +12,7 @@ sudo apt install unzip
 sudo apt install git python-setuptools python-dev python-virtualenv curl rabbitmq-server
 sudo apt install mysql-server mysql-client libmysqlclient-dev
 sudo apt install redis-server
+sudo apt-get install chrome-browser
 redis-server --daemonize yes
 
 #
