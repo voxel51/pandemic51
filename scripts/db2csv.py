9bf600e5923b2a0eb6b6ca947118c7ea7c1b9d77
Tyler Ganter <tyler@voxel51.com>
2020-04-01 18:13:49 -0400

    params

--- scripts/db2csv.py
+++ scripts/db2csv.py
@@ -13,6 +13,35 @@ import pandas as pd
 import pandemic51.core.database as pand
 
 
+##############
+# PARAMETERS #
+##############
+
+
+# names of streams to save to CSV
+streams_to_keep = [
+    "abbey_road",
+    "dublin",
+    "fort_lauderdale",
+    "new_jersey",
+    "new_orleans",
+    "prague",
+    "time_square"
+]
+
+# names of columns to keep (in this order)
+columns_to_keep = [
+    "stream_name",
+    "timestamp",    # datetime transformed to epoch time
+    "count"
+]
+
+
+########
+# CODE #
+########
+
+
 # load the database table to a DataFrame object
 columns = ["id", "stream_name", "datetime", "data_path", "labels_path",
            "count"]
@@ -20,15 +49,12 @@ data = pand.query_stream_history()
 df = pd.DataFrame(data, columns=columns)
 
 # drop unwanted streams
-streams_to_keep = ["abbey_road", "dublin", "fort_lauderdale", "new_jersey",
-                   "new_orleans", "prague", "time_square"]
 df = df[df.stream_name.isin(streams_to_keep)]
 
 # transform the datetime to an integer timestamp series
 df["timestamp"] = df["datetime"].transform(lambda t: int(t.timestamp()))
 
 # drop unwanted columns
-columns_to_keep = ["stream_name", "timestamp", "count"]
 df = df[columns_to_keep]
 
 # write to CSV
