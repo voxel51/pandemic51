650546d7a7352b5cd8d378b00e217b53b87de6ee
Tyler Ganter <tyler@voxel51.com>
2020-04-01 16:49:44 -0400

    remove unwanted streams

--- scripts/db2csv.py
+++ scripts/db2csv.py
@@ -19,12 +19,17 @@ columns = ["id", "stream_name", "datetime", "data_path", "labels_path",
 data = pand.query_stream_history()
 df = pd.DataFrame(data, columns=columns)
 
+# drop unwanted streams
+streams_to_keep = ["abbey_road", "dublin", "fort_lauderdale", "new_jersey",
+                   "new_orleans", "prague", "time_square"]
+df = df[df.stream_name.isin(streams_to_keep)]
+
 # transform the datetime to an integer timestamp series
 df["timestamp"] = df["datetime"].transform(lambda t: int(t.timestamp()))
 
 # drop unwanted columns
 columns_to_keep = ["stream_name", "timestamp", "count"]
-df = df.drop(columns=[col for col in columns if col not in columns_to_keep])
+df = df[columns_to_keep]
 
 # write to CSV
 df.to_csv("counts.csv", index=False)
