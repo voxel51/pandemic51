079cb2092d1052243f8c194a7ba45a777f88f669
Eric Hofesmann <ehofesmann@gmail.com>
2020-04-02 10:40:42 -0400

    Make code reusable

--- tests/download_and_store/example_mjpg_stream_download.py
+++ tests/download_and_store/example_mjpg_stream_download.py
@@ -12,6 +12,18 @@ import os
 import ffmpy
 
 import eta.core.utils as etau
+import pandemic51.core.streaming as pans
+
+
+def stream_frames(input_video, base_output_dir):
+    output_frames_path = os.path.join("out", base_output_dir, "%Y-%m-%d_%H-%M-%S.jpg")
+    etau.ensure_basedir(output_frames_path)
+    
+    ffmpy.FFmpeg(
+        inputs={input_video: None},
+        outputs={output_frames_path: "-strftime 1"},
+    ).run()
+
 
 # Ann Arbor
 input_video = "http://141.213.137.162/mjpg/video.mjpg"
@@ -21,12 +33,15 @@ base_output_dir = "ann_arbor"
 #input_video = "http://216.8.159.21/axis-cgi/mjpg/video.cgi"
 #base_output_dir = "detroit"
 
-output_frames_path = os.path.join("out", base_output_dir, "%Y-%m-%d_%H-%M-%S.jpg")
+
+# Capture the current frame of the stream
+output_frames_path = os.path.join("out", base_output_dir,
+        str(datetime.now()).replace(" ", "_")+".jpg")
 etau.ensure_basedir(output_frames_path)
 
-ffmpy.FFmpeg(
-    inputs={input_video: None},
-    outputs={output_frames_path: "-strftime 1"},
-).run()
+pans.sample_first_frame(input_video, output_frames_path)
 
+
+## Continually stream frames as images
+#stream_frames(input_video, base_output_dir)
 
