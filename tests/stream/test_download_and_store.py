cc4e3b0a13655054e951d97bbe92d43e80653174
Tyler Ganter <tyler@voxel51.com>
2020-04-02 17:00:25 -0400

    reorg

--- tests/stream/test_download_and_store.py
+++ tests/stream/test_download_and_store.py
@@ -7,13 +7,9 @@ voxel51.com
 import pandemic51.core.streaming as pans
 
 
-STREAM_NAMES = [
-    "time_square", "chicago", "new_orleans", "fort_lauderdale",
-    "new_jersey", "abbey_road", "dublin", "prague"]
-
 OUT_BASEDIR = "out"
 
 
-for stream_name in STREAM_NAMES:
+for stream_name in pans.Stream.get_stream_names():
     stream = pans.Stream.from_stream_name(stream_name)
     stream.download_image_and_store(outdir=OUT_BASEDIR)
