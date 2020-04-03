62801b9ec8c36c7c778064d25bc4318ca3369950
Benjamin Kane <ben@voxel51.com>
2020-04-03 16:50:23 -0400

    orange beta/new tags

--- web/src/components/cityCard.js
+++ web/src/components/cityCard.js
@@ -88,12 +88,14 @@ export default function CityCard(props) {
                 {LOCATIONS[props.cityId]}
               </Typography>
               {NEW[props.cityId] || BETA[props.cityId] ? (
-                <Card square className={classes.chip}>
+                <Card square className={classes.chip}
+                      style={{ background: "rgb(255, 109, 4)", color: "#fff" }}
+                >
                   <CardContent style={{ padding: 0 }}>
                     <Typography
                       variant="body1"
                       component="p"
-                      style={{ color: "rgb(255, 109, 4)" }}
+                      style={{fontWeight: "bold"}}
                     >
                       {NEW[props.cityId] ? <>NEW</> : <>BETA</>} FEED
                     </Typography>
