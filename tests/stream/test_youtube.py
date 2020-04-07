1beaca6c0d8920222eef20f8c37ad0b613984462
Jason Corso <jjcorso@umich.edu>
2020-04-06 21:41:30 -0400

    adding test for the youtube streams

---
+++ tests/stream/test_youtube.py
@@ -0,0 +1,27 @@
+'''
+Test script for `pandemic51.core.streaming.YouTubeStream`
+
+Copyright 2020 Voxel51, Inc.
+voxel51.com
+'''
+import pandemic51.core.streaming as pans
+
+
+OUT_BASEDIR = "/tmp"
+
+
+print("Creating the stream")
+
+stream = pans.Stream.from_json("./youtube-test.json")
+
+print(stream)
+
+print("The livestream URL for embedding is: %s" % stream.get_live_stream_url())
+
+print("Getting an image")
+
+path, dt = stream.download_image_and_store(outdir=OUT_BASEDIR)
+
+print("Stored it at %s" % path)
+
+print("Done")
