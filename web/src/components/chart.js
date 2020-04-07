5848a04ed7343935d088acea4cc6f490f376a48e
Benjamin Kane <ben@voxel51.com>
2020-04-07 17:14:48 -0400

    ensure all points all visible

--- web/src/components/chart.js
+++ web/src/components/chart.js
@@ -224,7 +224,7 @@ class Chart extends Component {
               <YAxis
                 dataKey="pdi"
                 name="PDI"
-                domain={[0, d => Math.max(d.toFixed(0) + 1, 10)]}
+                domain={[0, d => Math.max(Math.min(100, (d + 2)).toFixed(0), 10)]}
                 width={25}
                 label={
                   <Label
