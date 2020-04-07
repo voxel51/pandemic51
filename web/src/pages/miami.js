def6a1510eb672ba513fdd2b7a1d270fc18beb4d
Benjamin Kane <ben@voxel51.com>
2020-04-07 14:26:17 -0400

    adding to config and creating page

---
+++ web/src/pages/miami.js
@@ -0,0 +1,20 @@
+/**
+ * Chicago live stream.
+ *
+ * Copyright 2020, Voxel51, Inc.
+ * voxel51.com
+ */
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MiamiPage = () => (
+  <>
+    <SEO/>
+    <Layout city="miami"/>
+  </>
+)
+
+export default MiamiPage
