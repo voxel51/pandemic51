5cd9589e25297c603f1154208fb4c1bed72df1a2
Jason Corso <jjcorso@umich.edu>
2020-04-09 11:18:30 -0400

    converting redaction to callable script

--- tests/detections/test_redaction.py
+++ tests/detections/test_redaction.py
@@ -16,6 +16,7 @@ from builtins import *
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
+import argparse
 import logging
 from math import ceil
 
@@ -32,74 +33,70 @@ import eta.core.objects as etao
 logger = logging.getLogger(__name__)
 
 
-image_path = \
-"/scratch/jason-data/pandemic51/dublin_imgs/2020-03-16_1584359102236_1_11:45:02.jpg"
-label_path = \
-"/scratch/jason-data/pandemic51/dublin_labels/2020-03-16_1584359102236_1_11:45:02.json"
-
-
 def lowpass(image, sigma=3.0):
     r = scipy.ndimage.gaussian_filter(image[...,0], sigma)
     g = scipy.ndimage.gaussian_filter(image[...,1], sigma)
     b = scipy.ndimage.gaussian_filter(image[...,2], sigma)
     return np.dstack((r, g, b))
 
-image = etai.to_float(etai.read(image_path))
-gauss = lowpass(image)
-labels = etai.ImageLabels.from_json(label_path)
+def redact(image_path, label_path):
+    image = etai.to_float(etai.read(image_path))
+    gauss = lowpass(image)
+    labels = etai.ImageLabels.from_json(label_path)
 
-schema_filter = etao.ObjectContainerSchema()
-schema_filter.add_object_label("person")
+    schema_filter = etao.ObjectContainerSchema()
+    schema_filter.add_object_label("person")
 
-fig, ax = plt.subplots(1)
+    fig, ax = plt.subplots(1)
 
-if labels.has_objects:
-    labels.objects.filter_by_schema(schema_filter)
-    logger.debug("Rendering %d objects", len(labels.objects))
-    for obj in labels.objects:
-        print(obj)
-        if not obj.has_bounding_box:
-            logger.debug("object without bounding box.")
-            continue
+    if labels.has_objects:
+        labels.objects.filter_by_schema(schema_filter)
+        logger.debug("Rendering %d objects", len(labels.objects))
+        for obj in labels.objects:
+            if not obj.has_bounding_box:
+                logger.debug("object without bounding box.")
+                continue
 
-        print(obj.bounding_box)
+            boxtlx, boxtly = obj.bounding_box.top_left.coords_in(img=image)
+            boxbrx, boxbry = obj.bounding_box.bottom_right.coords_in(img=image)
+            boxw = boxbrx-boxtlx
+            boxh = boxbry-boxtly
 
-        boxtlx, boxtly = obj.bounding_box.top_left.coords_in(img=image)
-        boxbrx, boxbry = obj.bounding_box.bottom_right.coords_in(img=image)
-        boxw = boxbrx-boxtlx
-        boxh = boxbry-boxtly
+            headw = int(ceil(0.6*boxw))
+            headh = int(ceil(0.16*boxh))
+            headtlx = int(ceil(boxtlx+0.2*boxw))
+            headtly = int(boxtly)
+            headbrx = headtlx+headw
+            headbry = headtly+headh
 
-        headw = int(ceil(0.6*boxw))
-        headh = int(ceil(0.16*boxh))
-        headtlx = int(ceil(boxtlx+0.2*boxw))
-        headtly = int(boxtly)
-        headbrx = headtlx+headw
-        headbry = headtly+headh
+            box = pat.Rectangle((boxtlx, boxtly), boxw, boxh,
+                                linewidth=0.25,
+                                edgecolor='r',
+                                facecolor='none')
+            ax.add_patch(box)
 
-        print(boxtlx, boxtly)
-        print(boxw, boxh)
-        print(headtlx, headtly)
-        print(headw, headh)
+            head = pat.Rectangle((headtlx, headtly), headw, headh,
+                                 linewidth=0.5,
+                                 edgecolor='b',
+                                 facecolor='none')
 
-        box = pat.Rectangle((boxtlx, boxtly), boxw, boxh,
-                            linewidth=0.25,
-                            edgecolor='r',
-                            facecolor='none')
-        ax.add_patch(box)
+            ax.add_patch(head)
 
-        head = pat.Rectangle((headtlx, headtly), headw, headh,
-                             linewidth=0.5,
-                             edgecolor='b',
-                             facecolor='none')
+            image[headtly:headbry, headtlx:headbrx, :] = \
+                    gauss[headtly:headbry, headtlx:headbrx, :]
 
-        ax.add_patch(head)
+    etai.write(image*255, "/tmp/redacted.jpg")
 
-        image[headtly:headbry, headtlx:headbrx, :] = \
-                gauss[headtly:headbry, headtlx:headbrx, :]
+    ax.imshow(image)
+    plt.show()
 
-etai.write(image*255, "/tmp/redacted.jpg")
+if __name__ == "__main__":
 
-ax.imshow(image)
-plt.show()
+    parser = argparse.ArgumentParser(prog="test_redaction.py", add_help=True)
+    parser.add_argument("image_path", help="path to the source image")
+    parser.add_argument("label_path", help="path to the labels on the source")
+    args = parser.parse_args()
+
+    redact(args.image_path, args.label_path)
 
 
