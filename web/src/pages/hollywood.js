3de138e1bb6ece62246ac87de9e0fd0766f19a84
Benjamin Kane <ben@voxel51.com>
2020-04-15 17:09:02 -0400

    adding hollywood

---
+++ web/src/pages/hollywood.js
@@ -0,0 +1,20 @@
+/**
+ * Dublin live stream.
+ *
+ * Copyright 2020, Voxel51, Inc.
+ * voxel51.com
+ */
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const HollywoodPage = () => (
+  <>
+    <SEO/>
+    <Layout city="hollywood"/>
+  </>
+)
+
+export default HollywoodPage
