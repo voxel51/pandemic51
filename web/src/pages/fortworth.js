031ae232c0bd1dfa33e16a0c02f0bbd3708ed376
Benjamin Kane <ben@voxel51.com>
2020-04-24 16:22:20 -0400

    adding cadiz and fort worth pages

---
+++ web/src/pages/fortworth.js
@@ -0,0 +1,20 @@
+/**
+ * Fort Worth live stream.
+ *
+ * Copyright 2020, Voxel51, Inc.
+ * voxel51.com
+ */
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FortWorthPage = () => (
+  <>
+    <SEO />
+    <Layout city="fortworth" />
+  </>
+)
+
+export default FortWorthPage
